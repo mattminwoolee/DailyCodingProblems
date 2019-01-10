5f283b25c6ec7ce0cb566bbdee6de0ea5001f547
Matthew Lee <mattminwoolee@gmail.com>
2019-01-10 13:49:43 -0800

    Problem 35 - incomplete

--- 35_SwapRGB.js
+++ 35_SwapRGB.js
@@ -13,10 +13,49 @@ For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should beco
 I: [ Rs or Gs or Bs ]
 O: [ Rs or Gs or Bs ] in order R -> G -> B
 
-['G', 'B', 'R', 'R', 'B', 'R', 'G']
-  ^ 
-*/
 
+['G', 'B', 'R', 'R', 'B', 'R', 'G']
+  
+// since theres only three, we can focus on pushing the B's to the right and the R's to the left and Gs will naturally align itself
+
+R index = 0;
+G index = length-1;
+
+iterate over the array linearly and if you come across an R, place it into the first index, swap out with the el residing there
+
+*/
+// 1st attempt - solved with pia's help but incomplete
 function rgbSwap(arr) {
-	
+	let r_index = 0,
+		b_index = arr.length-1;
+
+	for (let i = 0; i < arr.length; i++ ) {
+		if ( arr[i] === 'R') {
+			// first check if the r_index already has a 'R' or not
+			while ( arr[r_index] === 'R' ) {
+				r_index++;
+			}
+
+			let temp = arr[r_index];
+			arr[r_index] = arr[i];
+			arr[i] = temp;
+
+			r_index++;
+		} else if ( arr[i] === 'B') {
+			// first check if the b_index already has a 'B' or not
+			while ( arr[b_index] === 'B' ) {
+				b_index--;
+			}
+
+			let temp = arr[b_index];
+			arr[b_index] = arr[i];
+			arr[i] = temp;
+
+			b_index--;
+		}
+	}
+
+	return arr;
 }
+
+console.log(rgbSwap(['G', 'B', 'R', 'R', 'B', 'R', 'G']))
